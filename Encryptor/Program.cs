9b7c995536f064a9daadd4684ac35eb7ea449af5
Szabi <nagysz@kkszki.hu>
2025-10-16 12:05:24 +0200

    Add project files.

---
+++ Encryptor/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace feladat2
+{
+    internal class Program
+    {
+        static int kodSzam = 2459;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adja meg a titkosítandó szöveget:");
+            string szoveg = Console.ReadLine();
+
+            string encrypted = Encryptor(szoveg, kodSzam);
+            Console.WriteLine($"Titkosított szöveg: {encrypted}");
+            Console.ReadLine();
+        }
+        static string Encryptor(string szoveg, int kodSzam)
+        {
+            string abc = "abcdefghijklmnopqrstuvwxyz";
+            string kodString = kodSzam.ToString().PadLeft(4, '0');
+
+            string eredmeny = "";
+
+            for (int i = 0; i < szoveg.Length; i++)
+            {
+                char betu = char.ToLower(szoveg[i]);
+                int eltol = kodString[i % 4] - '0';
+
+                if (abc.Contains(betu))
+                {
+                    int eredetiIndex = abc.IndexOf(betu);
+                    int ujIndex = (eredetiIndex + eltol) % abc.Length;
+                    eredmeny += abc[ujIndex];
+                }
+                else
+                {
+                    eredmeny += szoveg[i];
+                }
+            }
+
+            return eredmeny;
+        }
+
+    }
+}
---
+++ Encryptor/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Encryptor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Encryptor")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("16caa1a6-11c3-45a0-bb29-ba6c701c0d1b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
